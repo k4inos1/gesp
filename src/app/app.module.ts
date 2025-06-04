6b3520a33778dbe904a525cf1b364819b8e627a9
k4inos1 <ricardosanhueza09@icloud.com>
2025-06-04 00:18:07 -0400

    instalacion de firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthInterceptor } from './core/interceptors/auth.interceptor';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 @NgModule({
   declarations: [
@@ -22,7 +30,21 @@ import { AuthInterceptor } from './core/interceptors/auth.interceptor';
     AppRoutingModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    provideFirebaseApp(() => initializeApp({"projectId":"gesapp-6fe80","appId":"1:884687252315:web:fe17c98f8d5ff9364c487e","storageBucket":"gesapp-6fe80.firebasestorage.app","apiKey":"AIzaSyAFCuTOFG2KYJICD2xNGTSj857FmZgtihI","authDomain":"gesapp-6fe80.firebaseapp.com","messagingSenderId":"884687252315","measurementId":"G-MMNRM12MH9"})),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideAppCheck(() => {
+      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+      const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+      return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    }),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideRemoteConfig(() => getRemoteConfig())
   ],
   bootstrap: [AppComponent]
 })
